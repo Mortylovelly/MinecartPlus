e58f6d8c705d525fc37b7b4b5cf6accd56ebb44b
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:37:26 +0300

    Update MinecartVisualMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
@@ -1,20 +1,20 @@
 package com.minecartmagic.mixin;
 
 import com.minecartmagic.ModEnchantments;
-import net.minecraft.entity.vehicle.MinecartEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.world.entity.vehicle.Minecart;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecartEntity.class)
+@Mixin(Minecart.class)
 public class MinecartVisualMixin {
     @Inject(method = "isGlowing", at = @At("RETURN"), cancellable = true)
     private void makeEnchantedCartGlow(CallbackInfoReturnable<Boolean> cir) {
-        MinecartEntity cart = (MinecartEntity) (Object) this;
-        ItemStack cartItem = cart.getPickBlockStack();
+        Minecart cart = (Minecart) (Object) this;
+        ItemStack cartItem = cart.getPickResult();
         
         if (cartItem.getItem() == Items.MINECART) {
             if (ModEnchantments.getTractionLevel(cartItem) > 0) {
