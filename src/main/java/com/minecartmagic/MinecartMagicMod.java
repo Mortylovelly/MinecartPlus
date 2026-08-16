596529baf920bd25b7345026f591b740b0dd8830
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:36:51 +0300

    Update MinecartMagicMod.java

--- src/main/java/com/minecartmagic/MinecartMagicMod.java
+++ src/main/java/com/minecartmagic/MinecartMagicMod.java
@@ -1,7 +1,7 @@
 package com.minecartmagic;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +15,7 @@ public class MinecartMagicMod implements ModInitializer {
         LOGGER.info("Minecart Magic loaded!");
     }
 
-    public static Identifier id(String path) {
-        return Identifier.of(MOD_ID, path);
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 }
