102438622588118499d975f553dad0ef4e805977
Morty <zaharzvagin254@gmail.com>
2026-08-16 18:27:54 +0300

    Update MinecartVisualMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
@@ -2,7 +2,18 @@ package com.minecartmagic.mixin;
 
 import net.minecraft.entity.vehicle.MinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecartEntity.class)
-public abstract class MinecartVisualMixin {
+public class MinecartVisualMixin {
+    @Inject(method = "isGlowing", at = @At("RETURN"), cancellable = true)
+    private void makeEnchantedCartGlow(CallbackInfoReturnable<Boolean> cir) {
+        MinecartEntity cart = (MinecartEntity) (Object) this;
+        int level = cart.getPersistentData().getInt("traction_level");
+        if (level > 0) {
+            cir.setReturnValue(true);
+        }
+    }
 }
