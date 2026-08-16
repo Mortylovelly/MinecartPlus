ec7f2d4f754c2d7e01b0a4e1bb58d2f2233c5d63
Morty <zaharzvagin254@gmail.com>
2026-08-16 19:33:14 +0300

    Update MinecartSpeedMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
@@ -1,7 +1,6 @@
 package com.minecartmagic.mixin;
 
-import com.minecartmagic.enchantment.ModEnchantments;
-import net.minecraft.enchantment.EnchantmentHelper;
+import com.minecartmagic.ModEnchantments;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,12 +15,7 @@ public class MinecartSpeedMixin {
     private void minecartmagic$modifyMaxSpeed(ServerWorld world, CallbackInfoReturnable<Double> cir) {
         AbstractMinecartEntity minecart = (AbstractMinecartEntity) (Object) this;
 
-        int level = EnchantmentHelper.getLevel(
-                world.getRegistryManager()
-                        .getOrThrow(net.minecraft.registry.RegistryKeys.ENCHANTMENT)
-                        .getOrThrow(ModEnchantments.TRACTION_KEY),
-                minecart.getPickBlockStack()
-        );
+        int level = ModEnchantments.getTractionLevel(minecart.getPickBlockStack());
 
         if (level > 0) {
             double multiplier = 1.0 + (0.3 * level);
