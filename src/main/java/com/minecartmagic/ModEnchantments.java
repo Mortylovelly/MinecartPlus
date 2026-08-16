3396984b5ebd39ee9ad5d4cb6ab45e50cb1bc551
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:18:36 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -3,8 +3,8 @@ package com.minecartmagic;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.TagKey;
@@ -15,7 +15,7 @@ public class ModEnchantments {
     public static final TagKey<Enchantment> TRACTION_TAG = TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction_tag"));
 
     public static final Enchantment TRACTION = Registry.register(
-        RegistryKeys.ENCHANTMENT,
+        Registries.ENCHANTMENT,
         Identifier.of("minecartmagic", "traction"),
         new Enchantment(
             new Enchantment.Definition(
@@ -23,16 +23,15 @@ public class ModEnchantments {
                 TRACTION_TAG,
                 1, // weight
                 3, // max level
-                Enchantment.minCost(10),
-                Enchantment.maxCost(20),
+                Enchantment.constantCost(10),
+                Enchantment.constantCost(20),
                 10, // anvil cost
-                EquipmentSlot.MAINHAND
+                new EquipmentSlot[]{EquipmentSlot.MAINHAND}
             )
         )
     );
 
-    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = Registry.getEntry(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction"));
+    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = Registries.ENCHANTMENT.getEntry(TRACTION).orElseThrow();
 
-    public static void init() {
-    }
+    public static void init() {}
 }
