fb40fa201c5be474663c6a566c10b4a230eb7d07
Morty <zaharzvagin254@gmail.com>
2026-08-16 17:27:38 +0300

    Update MinecartSpeedMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
@@ -1,20 +1,20 @@
 package com.minecartmagic.mixin;
 
 import com.minecartmagic.ModEnchantments;
-import net.minecraft.world.entity.vehicle.Minecart;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.entity.vehicle.MinecartEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Minecart.class)
+@Mixin(MinecartEntity.class)
 public class MinecartSpeedMixin {
     @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
     private void modifyMaxSpeed(CallbackInfoReturnable<Double> cir) {
-        Minecart cart = (Minecart) (Object) this;
-        ItemStack cartItem = cart.getPickResult();
+        MinecartEntity cart = (MinecartEntity) (Object) this;
+        ItemStack cartItem = cart.getPickBlockStack();
         
         if (cartItem.getItem() == Items.MINECART) {
             int level = ModEnchantments.getTractionLevel(cartItem);
