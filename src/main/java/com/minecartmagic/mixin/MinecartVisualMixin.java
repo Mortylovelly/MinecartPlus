268f3b6b8a0073c74a695d93a127ae384da8ac00
Morty <zaharzvagin254@gmail.com>
2026-08-16 17:54:25 +0300

    Update MinecartVisualMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
@@ -1,25 +1,7 @@
 package com.minecartmagic.mixin;
 
-import com.minecartmagic.ModEnchantments;
-import net.minecraft.entity.vehicle.MinecartEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecartEntity.class)
-public class MinecartVisualMixin {
-    @Inject(method = "isGlowing", at = @At("RETURN"), cancellable = true)
-    private void makeEnchantedCartGlow(CallbackInfoReturnable<Boolean> cir) {
-        MinecartEntity cart = (MinecartEntity) (Object) this;
-        ItemStack cartItem = cart.getPickBlockStack();
-        
-        if (cartItem.getItem() == Items.MINECART) {
-            if (ModEnchantments.getTractionLevel(cartItem) > 0) {
-                cir.setReturnValue(true);
-            }
-        }
-    }
+@Mixin(targets = "net.minecraft.entity.vehicle.MinecartEntity")
+public abstract class MinecartVisualMixin {
 }
