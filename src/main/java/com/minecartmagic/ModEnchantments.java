057dba4360052bcf25c5cd3a9a3629d5ef68bf6b
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:27:33 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -1,38 +1,42 @@
 package com.minecartmagic;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.entry.RegistryEntryList;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModEnchantments {
-    public static final TagKey<Item> ENCHANTABLE_MINECART = TagKey.of(RegistryKeys.ITEM, Identifier.of("minecartmagic", "enchantable_minecart"));
-    public static final TagKey<Enchantment> TRACTION_TAG = TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction_tag"));
-
-    public static final Enchantment TRACTION = Registry.register(
-        RegistryKeys.ENCHANTMENT,
-        Identifier.of("minecartmagic", "traction"),
-        new Enchantment(
-            new Enchantment.Definition(
-                Registries.ITEM.getOrCreateEntryList(ENCHANTABLE_MINECART),
-                TRACTION_TAG,
-                1, // weight
-                3, // max level
-                Enchantment.constantCost(10),
-                Enchantment.constantCost(20),
-                10, // anvil cost
-                new EquipmentSlot[]{EquipmentSlot.MAINHAND}
-            )
+    public static final Enchantment TRACTION = new Enchantment(
+        new Enchantment.Definition(
+            null,
+            1,
+            3,
+            Enchantment.Cost.constant(10),
+            Enchantment.Cost.constant(20),
+            10,
+            new EquipmentSlot[]{EquipmentSlot.MAINHAND}
         )
     );
 
-    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = Registry.getEntry(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction"));
+    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = Registry.registerReference(
+        RegistryKeys.ENCHANTMENT,
+        Identifier.of("minecartmagic", "traction"),
+        TRACTION
+    );
 
-    public static void init() {}
+    public static void init() {
+    }
+
+    public static boolean isMinecart(ItemStack stack) {
+        return stack.getItem() == Items.MINECART;
+    }
+
+    public static int getTractionLevel(ItemStack stack) {
+        return EnchantmentHelper.getLevel(TRACTION_ENTRY, stack);
+    }
 }
