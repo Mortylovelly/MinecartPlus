8dcec4da8fd481f35fe1b4a95e24d95cc51f8e67
Morty <zaharzvagin254@gmail.com>
2026-08-16 19:41:24 +0300

    Update MinecartSpeedMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
@@ -1,8 +1,9 @@
 package com.minecartmagic.mixin;
 
-import com.minecartmagic.ModEnchantments;
+import com.minecartmagic.enchantment.ModEnchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.registry.RegistryKeys;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MinecartSpeedMixin {
 
     @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
-    private void minecartmagic$modifyMaxSpeed(ServerWorld world, CallbackInfoReturnable<Double> cir) {
+    private void minecartmagic$modifyMaxSpeed(CallbackInfoReturnable<Double> cir) {
         AbstractMinecartEntity minecart = (AbstractMinecartEntity) (Object) this;
 
-        int level = ModEnchantments.getTractionLevel(minecart.getPickBlockStack());
+        var world = minecart.getWorld();
+
+        int level = EnchantmentHelper.getLevel(
+                world.getRegistryManager()
+                        .getOrThrow(RegistryKeys.ENCHANTMENT)
+                        .getOrThrow(ModEnchantments.TRACTION_KEY),
+                minecart.getPickBlockStack()
+        );
 
         if (level > 0) {
             double multiplier = 1.0 + (0.3 * level);
