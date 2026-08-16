08eb0dc071609076fe187ad5b1e416a8c70357db
Morty <zaharzvagin254@gmail.com>
2026-08-16 15:59:06 +0300

    Create MinecartSpeedMixin.java

---
+++ src/main/java/com/minecartmagic/mixin/MinecartSpeedMixin.java
@@ -0,0 +1,28 @@
+package com.minecartmagic.mixin;
+
+import com.minecartmagic.ModEnchantments;
+import net.minecraft.entity.vehicle.MinecartEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecartEntity.class)
+public class MinecartSpeedMixin {
+    @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
+    private void modifyMaxSpeed(CallbackInfoReturnable<Double> cir) {
+        MinecartEntity cart = (MinecartEntity) (Object) this;
+        ItemStack cartItem = cart.getPickBlockStack();
+        
+        if (cartItem.getItem() == Items.MINECART) {
+            int level = cartItem.getEnchantments().getLevel(ModEnchantments.TRACTION);
+            if (level > 0) {
+                double baseSpeed = cir.getReturnValue();
+                double multiplier = 1.0 + (level * 0.3); // +30%, +60%, +90%
+                cir.setReturnValue(baseSpeed * multiplier);
+            }
+        }
+    }
+}
