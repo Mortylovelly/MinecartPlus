579da26adf6acaa7ac475407fa6d0525a7a8b3f6
Morty <zaharzvagin254@gmail.com>
2026-08-16 22:58:50 +0300

    Update MinecartMaxSpeedMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartMaxSpeedMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartMaxSpeedMixin.java
@@ -2,6 +2,7 @@ package com.minecartmagic.mixin;
 
 import com.minecartmagic.ModEnchantments;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.MinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,23 +19,43 @@ public abstract class MinecartMaxSpeedMixin {
     private void minecartmagic$increaseMaxSpeed(
             CallbackInfoReturnable<Double> cir
     ) {
-        AbstractMinecartEntity minecart =
-                (AbstractMinecartEntity) (Object) this;
 
-        int level = ModEnchantments.getTractionLevel(minecart);
+        /*
+         * Mixin находится на AbstractMinecartEntity,
+         * поэтому проверяем, что это именно обычная
+         * rideable MinecartEntity.
+         */
+        if (!((Object) this instanceof MinecartEntity minecart)) {
+            return;
+        }
 
+        int level =
+                ModEnchantments.getTractionLevel(minecart);
+
+        /*
+         * Без зачарования абсолютно ничего
+         * не меняем в ванильной вагонетке.
+         */
         if (level <= 0) {
             return;
         }
 
-        double vanillaSpeed = cir.getReturnValue();
+        double vanillaSpeed =
+                cir.getReturnValue();
 
+        /*
+         * Тяга I   = 1.5x
+         * Тяга II  = 2.0x
+         * Тяга III = 2.5x
+         */
         double multiplier = switch (level) {
             case 1 -> 1.50D;
             case 2 -> 2.00D;
             default -> 2.50D;
         };
 
-        cir.setReturnValue(vanillaSpeed * multiplier);
+        cir.setReturnValue(
+                vanillaSpeed * multiplier
+        );
     }
 }
