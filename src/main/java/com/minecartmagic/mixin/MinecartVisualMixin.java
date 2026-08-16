a29b9f361ff5072e06d1cf05971971b97f5c75f6
Morty <zaharzvagin254@gmail.com>
2026-08-16 18:47:46 +0300

    Update MinecartVisualMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartVisualMixin.java
@@ -1,23 +1,15 @@
 package com.minecartmagic.mixin;
 
 import net.minecraft.entity.vehicle.MinecartEntity;
-import net.minecraft.nbt.NbtCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecartEntity.class)
 public class MinecartVisualMixin {
-    @Inject(method = "isGlowing", at = @At("RETURN"), cancellable = true)
-    private void makeEnchantedCartGlow(CallbackInfoReturnable<Boolean> cir) {
-        MinecartEntity cart = (MinecartEntity) (Object) this;
-        NbtCompound nbt = cart.getData();
-        if (nbt != null && nbt.contains("traction_level")) {
-            int level = nbt.getInt("traction_level");
-            if (level > 0) {
-                cir.setReturnValue(true);
-            }
-        }
+
+    @Inject(method = "setGlowing", at = @At("TAIL"))
+    private void onSetGlowing(boolean glowing, CallbackInfo ci) {
     }
 }
