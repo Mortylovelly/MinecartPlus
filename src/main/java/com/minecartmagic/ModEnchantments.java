10b86222275c7dcb13a5ef99130c75db979447e9
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:05:17 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -1,45 +1,35 @@
 package com.minecartmagic;
 
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.EnchantmentTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.registry.BuiltInRegistries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModEnchantments {
+    public static final TagKey<Item> ENCHANTABLE_MINECART = TagKey.of(RegistryKeys.ITEM, Identifier.of("minecartmagic", "enchantable_minecart"));
+
     public static final Enchantment TRACTION = Registry.register(
-        Registries.ENCHANTMENT,
+        BuiltInRegistries.ENCHANTMENT,
         Identifier.of("minecartmagic", "traction"),
-        new TractionEnchantment()
+        new Enchantment(
+            new Enchantment.Properties(
+                ENCHANTABLE_MINECART, // supported items tag
+                TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction_tag")), // primary tag
+                1, // min level
+                3, // max level
+                Enchantment.constantCost(10), // min cost
+                Enchantment.constantCost(20), // max cost
+                10 // anvil cost
+            )
+        )
     );
 
+    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = BuiltInRegistries.ENCHANTMENT.getEntry(TRACTION);
+
     public static void init() {
-    }
-
-    public static class TractionEnchantment extends Enchantment {
-        public TractionEnchantment() {
-            super(Enchantment.properties(
-                TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of("minecartmagic", "traction_tag")),
-                1,
-                3,
-                Enchantment.constantCost(10),
-                Enchantment.constantCost(20),
-                10
-            ));
-        }
-
-        @Override
-        public boolean isAcceptableItem(net.minecraft.item.ItemStack stack) {
-            return stack.getItem() == net.minecraft.item.Items.MINECART;
-        }
-
-        @Override
-        public boolean isTreasure() {
-            return true;
-        }
     }
 }
