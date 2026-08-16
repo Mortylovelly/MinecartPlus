64ec969387ae238010bd9b9db925c2d9c76f6fe9
Morty <zaharzvagin254@gmail.com>
2026-08-16 17:37:14 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -1,17 +1,18 @@
 package com.minecartmagic;
 
+import net.minecraft.component.ComponentMap;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class ModEnchantments {
     );
 
     public static final Enchantment TRACTION = new Enchantment(
+        Text.translatable("enchantment.minecartmagic.traction"),
         new Enchantment.Definition(
             RegistryEntryList.of(RegistryEntry.of(Items.MINECART)),
             Optional.empty(),
@@ -33,14 +35,20 @@ public class ModEnchantments {
             new Enchantment.Cost(20, 0),
             10,
             List.of(AttributeModifierSlot.MAINHAND)
-        )
+        ),
+        RegistryEntryList.empty(),
+        ComponentMap.EMPTY
     );
 
     public static final RegistryEntry<Enchantment> TRACTION_ENTRY;
 
     static {
-        Registry.register(Registries.ENCHANTMENT, TRACTION_KEY, TRACTION);
-        TRACTION_ENTRY = Registries.ENCHANTMENT.getEntry(TRACTION_KEY);
+        Registry.register(
+            (Registry) Registry.REGISTRIES.get(RegistryKeys.ENCHANTMENT),
+            TRACTION_KEY.getValue(),
+            TRACTION
+        );
+        TRACTION_ENTRY = Registry.REGISTRIES.get(RegistryKeys.ENCHANTMENT).getEntry(TRACTION_KEY);
     }
 
     public static void init() {
