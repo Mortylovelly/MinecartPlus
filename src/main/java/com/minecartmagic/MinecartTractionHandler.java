0f442b0393940ca3d5e1279015517d83c967c915
Morty <zaharzvagin254@gmail.com>
2026-08-16 20:03:07 +0300

    Update MinecartTractionHandler.java

--- src/main/java/com/minecartmagic/MinecartTractionHandler.java
+++ src/main/java/com/minecartmagic/MinecartTractionHandler.java
@@ -3,67 +3,39 @@ package com.minecartmagic;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 
 public class MinecartTractionHandler {
 
-    private static final double SPEED_PER_LEVEL = 0.30;
-    private static final double MAX_MULTIPLIER = 2.50;
+    private static final double SPEED_MULTIPLIER_PER_LEVEL = 0.30D;
 
     public static void init() {
         ServerTickEvents.END_WORLD_TICK.register(MinecartTractionHandler::onWorldTick);
     }
 
     private static void onWorldTick(ServerWorld world) {
-        Box worldBox = new Box(
-                -30_000_000,
-                world.getBottomY(),
-                -30_000_000,
-                30_000_000,
-                world.getTopY(),
-                30_000_000
-        );
-
         for (AbstractMinecartEntity minecart : world.getEntitiesByClass(
                 AbstractMinecartEntity.class,
-                worldBox,
-                AbstractMinecartEntity::isAlive
+                minecart -> minecart.getBoundingBox().expand(0.1D),
+                minecart -> ModEnchantments.getTractionLevel(minecart.getPickBlockStack()) > 0
         )) {
-            int level = ModEnchantments.getTractionLevel(
-                    minecart.getPickBlockStack()
-            );
+            int level = ModEnchantments.getTractionLevel(minecart.getPickBlockStack());
 
             if (level <= 0) {
                 continue;
             }
 
-            double multiplier = 1.0 + SPEED_PER_LEVEL * level;
-            multiplier = Math.min(multiplier, MAX_MULTIPLIER);
+            double multiplier = 1.0D + (SPEED_MULTIPLIER_PER_LEVEL * level);
 
             Vec3d velocity = minecart.getVelocity();
 
-            double horizontalSpeed = velocity.horizontalLength();
-
-            if (horizontalSpeed < 0.000001) {
-                continue;
+            if (velocity.lengthSquared() > 0.000001D) {
+                minecart.setVelocity(
+                        velocity.x * multiplier,
+                        velocity.y,
+                        velocity.z * multiplier
+                );
             }
-
-            double vanillaMaxSpeed = minecart.getMaxSpeed();
-            double targetMaxSpeed = vanillaMaxSpeed * multiplier;
-
-            double targetSpeed = Math.min(
-                    horizontalSpeed * multiplier,
-                    targetMaxSpeed
-            );
-
-            double scale = targetSpeed / horizontalSpeed;
-
-            minecart.setVelocity(
-                    velocity.x * scale,
-                    velocity.y,
-                    velocity.z * scale
-            );
         }
     }
 }
