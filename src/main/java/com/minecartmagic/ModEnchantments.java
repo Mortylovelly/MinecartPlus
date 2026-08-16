a33a717351851380cea8aa7b0582411c1956a07f
Morty <zaharzvagin254@gmail.com>
2026-08-16 16:09:29 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -3,17 +3,17 @@ package com.minecartmagic;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.registry.BuiltInRegistries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModEnchantments {
     public static final TagKey<Item> ENCHANTABLE_MINECART = TagKey.of(RegistryKeys.ITEM, Identifier.of("minecartmagic", "enchantable_minecart"));
 
     public static final Enchantment TRACTION = Registry.register(
-        BuiltInRegistries.ENCHANTMENT,
+        Registries.ENCHANTMENT,
         Identifier.of("minecartmagic", "traction"),
         new Enchantment(
             new Enchantment.Properties(
@@ -28,7 +28,7 @@ public class ModEnchantments {
         )
     );
 
-    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = BuiltInRegistries.ENCHANTMENT.getEntry(TRACTION);
+    public static final RegistryEntry<Enchantment> TRACTION_ENTRY = Registries.ENCHANTMENT.getEntry(TRACTION);
 
     public static void init() {
     }
