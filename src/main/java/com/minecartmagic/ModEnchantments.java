2f910c460228a622905d0955ebd85fbc61fe35b2
Morty <zaharzvagin254@gmail.com>
2026-08-16 17:41:17 +0300

    Update ModEnchantments.java

--- src/main/java/com/minecartmagic/ModEnchantments.java
+++ src/main/java/com/minecartmagic/ModEnchantments.java
@@ -5,7 +5,7 @@ import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.Items;
-import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -42,8 +42,8 @@ public class ModEnchantments {
     public static final RegistryEntry<Enchantment> TRACTION_ENTRY;
 
     static {
-        Registry.register(BuiltinRegistries.ENCHANTMENT, TRACTION_KEY.getValue(), TRACTION);
-        TRACTION_ENTRY = BuiltinRegistries.ENCHANTMENT.getEntry(TRACTION_KEY);
+        Registry.register(Registries.ENCHANTMENT, TRACTION_KEY.getValue(), TRACTION);
+        TRACTION_ENTRY = Registries.ENCHANTMENT.getEntry(TRACTION_KEY);
     }
 
     public static void init() {
