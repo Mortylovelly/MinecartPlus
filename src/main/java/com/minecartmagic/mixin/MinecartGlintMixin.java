b45d96953a67936acd69358ecd110b567c88f92c
Morty <zaharzvagin254@gmail.com>
2026-08-17 20:48:07 +0300

    Update MinecartGlintMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartGlintMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartGlintMixin.java
@@ -1,39 +1,40 @@
 package com.minecartmagic.mixin;
 
 import com.minecartmagic.ModEnchantments;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.MinecartEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(MinecartEntityRenderer.class)
-public class MinecartGlintMixin {
+public abstract class MinecartGlintMixin {
 
-    @Inject(
+    @Shadow
+    protected EntityModel<AbstractMinecartEntity> model;
+
+    @ModifyArg(
             method = "render",
-            at = @At("HEAD")
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/model/Model;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V"
+            ),
+            index = 1
     )
-    private void minecartmagic$renderTractionGlint(
-            AbstractMinecartEntity minecart,
-            float yaw,
-            float tickDelta,
+    private VertexConsumer minecartmagic$applyTractionGlint(
+            VertexConsumer original,
             MatrixStack matrices,
-            VertexConsumerProvider vertexConsumers,
             int light,
-            CallbackInfo ci
+            int overlay
     ) {
-        int tractionLevel =
-                ModEnchantments.getTractionLevel(minecart);
-
-        if (tractionLevel <= 0) {
-            return;
-        }
-
-        // Glint будет добавлен следующим безопасным способом.
-        // Здесь намеренно пока ничего не рендерим.
+        return original;
     }
 }
