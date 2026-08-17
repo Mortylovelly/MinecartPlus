61d81aeee245e95b81c5ff3f7721ed93e2297c5c
Morty <zaharzvagin254@gmail.com>
2026-08-17 19:33:32 +0300

    Update MinecartGlintMixin.java

--- src/main/java/com/minecartmagic/mixin/MinecartGlintMixin.java
+++ src/main/java/com/minecartmagic/mixin/MinecartGlintMixin.java
@@ -1,49 +1,39 @@
 package com.minecartmagic.mixin;
 
 import com.minecartmagic.ModEnchantments;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.MinecartEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecartEntityRenderer.class)
-public abstract class MinecartGlintMixin<T extends AbstractMinecartEntity> {
-
-    @Shadow
-    protected EntityModel<T> model;
+public class MinecartGlintMixin {
 
     @Inject(
             method = "render(Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;III)V",
-                    ordinal = 0
-            )
+            at = @At("HEAD")
     )
     private void minecartmagic$renderTractionGlint(
-            T minecart,
+            AbstractMinecartEntity minecart,
             float yaw,
             float tickDelta,
             MatrixStack matrices,
-            VertexConsumerProvider vertexConsumers,
+            net.minecraft.client.render.VertexConsumerProvider vertexConsumers,
             int light,
             CallbackInfo ci
     ) {
-        if (ModEnchantments.getTractionLevel(minecart.getPickBlockStack()) <= 0) {
+        int tractionLevel = ModEnchantments.getTractionLevel(
+                minecart.getPickBlockStack()
+        );
+
+        if (tractionLevel <= 0) {
             return;
         }
 
-        VertexConsumer glintConsumer =
-                vertexConsumers.getBuffer(RenderLayer.getEntityGlint());
-
-        model.render(matrices, glintConsumer, light, 0, 0xFFFFFFFF);
+        // Пока только проверяем корректное подключение Mixin.
+        // Рендер самого glint добавим следующим безопасным этапом.
     }
 }
