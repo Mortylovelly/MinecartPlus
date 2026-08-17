486c31ab8a3d45082833b9fce4a77942b2fd16ed
Morty <zaharzvagin254@gmail.com>
2026-08-17 21:37:19 +0300

    Create MinecartDataMixin.java

---
+++ src/main/java/com/minecartmagic/mixin/MinecartDataMixin.java
@@ -0,0 +1,27 @@
+package com.minecartmagic.mixin;
+
+import com.minecartmagic.MinecartTractionData;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractMinecartEntity.class)
+public abstract class MinecartDataMixin {
+
+    @Inject(
+            method = "initDataTracker",
+            at = @At("TAIL")
+    )
+    private void minecartmagic$initTractionData(
+            DataTracker.Builder builder,
+            CallbackInfo ci
+    ) {
+        builder.add(
+                MinecartTractionData.TRACTION_LEVEL,
+                0
+        );
+    }
+}
