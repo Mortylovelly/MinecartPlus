b6516080997a62d0c71ff4d597630085dcd16481
Morty <zaharzvagin254@gmail.com>
2026-08-18 22:36:11 +0300

    Create SelfPropellingBoatSpeedMixin.java

---
+++ src/main/java/com/minecartmagic/mixin/SelfPropellingBoatSpeedMixin.java
@@ -0,0 +1,29 @@
+package com.minecartmagic.mixin;
+
+import com.minecartmagic.entity.SelfPropellingBoatEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BoatEntity.class)
+public abstract class SelfPropellingBoatSpeedMixin {
+
+    @Inject(
+            method = "updateVelocity",
+            at = @At("TAIL")
+    )
+    private void minecartmagic$applySelfPropellingBoatEngine(
+            CallbackInfo ci
+    ) {
+        BoatEntity boat =
+                (BoatEntity) (Object) this;
+
+        if (!(boat instanceof SelfPropellingBoatEntity selfPropellingBoat)) {
+            return;
+        }
+
+        selfPropellingBoat.applySelfPropulsion();
+    }
+}
