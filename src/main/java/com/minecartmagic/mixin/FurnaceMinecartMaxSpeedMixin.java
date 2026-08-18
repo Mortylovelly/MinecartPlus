49f3cb38c84a60153b2a38521c3e683f2548eb29
Morty <zaharzvagin254@gmail.com>
2026-08-18 18:56:01 +0300

    Create FurnaceMinecartMaxSpeedMixin.java

---
+++ src/main/java/com/minecartmagic/mixin/FurnaceMinecartMaxSpeedMixin.java
@@ -0,0 +1,46 @@
+package com.minecartmagic.mixin;
+
+import com.minecartmagic.ModEnchantments;
+import net.minecraft.entity.vehicle.FurnaceMinecartEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FurnaceMinecartEntity.class)
+public abstract class FurnaceMinecartMaxSpeedMixin {
+
+    @Inject(
+            method = "getMaxSpeed",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void minecartmagic$increaseFurnaceMinecartSpeed(
+            ServerWorld world,
+            CallbackInfoReturnable<Double> cir
+    ) {
+        FurnaceMinecartEntity minecart =
+                (FurnaceMinecartEntity) (Object) this;
+
+        int level =
+                ModEnchantments.getTractionLevel(minecart);
+
+        if (level <= 0) {
+            return;
+        }
+
+        double vanillaSpeed =
+                cir.getReturnValue();
+
+        double multiplier = switch (level) {
+            case 1 -> 1.75D;
+            case 2 -> 2.40D;
+            default -> 3.10D;
+        };
+
+        cir.setReturnValue(
+                vanillaSpeed * multiplier
+        );
+    }
+}
