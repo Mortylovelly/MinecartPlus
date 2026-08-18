5621046310959065fd73b3081d9c908a646d7a58
Morty <zaharzvagin254@gmail.com>
2026-08-18 21:30:54 +0300

    Update SelfPropellingBoatItem.java

--- src/main/java/com/minecartmagic/item/SelfPropellingBoatItem.java
+++ src/main/java/com/minecartmagic/item/SelfPropellingBoatItem.java
@@ -1,14 +1,15 @@
 package com.minecartmagic.item;
 
 import com.minecartmagic.ModEnchantments;
+import com.minecartmagic.ModEntities;
 import com.minecartmagic.entity.SelfPropellingBoatEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
@@ -37,7 +38,7 @@ public class SelfPropellingBoatItem extends Item {
                 );
 
         if (hit.getType()
-                == net.minecraft.util.hit.HitResult.Type.MISS) {
+                == HitResult.Type.MISS) {
 
             return TypedActionResult.pass(
                     stack
@@ -52,8 +53,7 @@ public class SelfPropellingBoatItem extends Item {
 
         SelfPropellingBoatEntity boat =
                 new SelfPropellingBoatEntity(
-                        com.minecartmagic.ModEntities
-                                .SELF_PROPELLING_BOAT,
+                        ModEntities.SELF_PROPELLING_BOAT,
                         world
                 );
 
@@ -67,6 +67,17 @@ public class SelfPropellingBoatItem extends Item {
                 user.getYaw()
         );
 
+        /*
+         * Пока существует только дубовая версия.
+         */
+        boat.setVariant(
+                net.minecraft.entity.vehicle.BoatEntity.Type.OAK
+        );
+
+        /*
+         * Переносим Tailwind с предмета
+         * на entity.
+         */
         int tailwindLevel =
                 ModEnchantments.getTailwindLevel(
                         stack
