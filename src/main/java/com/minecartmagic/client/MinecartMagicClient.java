038dabddf25d52863149fd6011ac0392632286fb
Morty <zaharzvagin254@gmail.com>
2026-08-18 23:59:54 +0300

    Update MinecartMagicClient.java

--- src/main/java/com/minecartmagic/client/MinecartMagicClient.java
+++ src/main/java/com/minecartmagic/client/MinecartMagicClient.java
@@ -3,8 +3,8 @@ package com.minecartmagic.client;
 import com.minecartmagic.ModEntities;
 import com.minecartmagic.screen.ModScreenHandlers;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 public class MinecartMagicClient
         implements ClientModInitializer {
@@ -12,17 +12,11 @@ public class MinecartMagicClient
     @Override
     public void onInitializeClient() {
 
-        /*
-         * Самоходная лодка.
-         */
         EntityRendererRegistry.register(
                 ModEntities.SELF_PROPELLING_BOAT,
                 SelfPropellingBoatRenderer::new
         );
 
-        /*
-         * Fuel GUI.
-         */
         HandledScreens.register(
                 ModScreenHandlers.SELF_PROPELLING_BOAT,
                 SelfPropellingBoatScreen::new
