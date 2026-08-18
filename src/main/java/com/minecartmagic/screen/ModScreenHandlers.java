4f6e2a168dbe58ca88e53e1eef2766f3759d0a0f
Morty <zaharzvagin254@gmail.com>
2026-08-18 23:57:42 +0300

    Update ModScreenHandlers.java

--- src/main/java/com/minecartmagic/screen/ModScreenHandlers.java
+++ src/main/java/com/minecartmagic/screen/ModScreenHandlers.java
@@ -1,11 +1,12 @@
 package com.minecartmagic.screen;
 
+import com.minecartmagic.MinecartMagicMod;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-
-import com.minecartmagic.MinecartMagicMod;
 
 public final class ModScreenHandlers {
 
@@ -14,7 +15,11 @@ public final class ModScreenHandlers {
             Integer
             > SELF_PROPELLING_BOAT =
             new ExtendedScreenHandlerType<>(
-                    (syncId, playerInventory, entityId) ->
+                    (
+                            syncId,
+                            playerInventory,
+                            entityId
+                    ) ->
                             new SelfPropellingBoatScreenHandler(
                                     syncId,
                                     playerInventory,
