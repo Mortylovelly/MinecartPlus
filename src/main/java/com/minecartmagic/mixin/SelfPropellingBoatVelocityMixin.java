43aa8e7974fc1a522f5617f4f9e2428210ce52a6
Morty <zaharzvagin254@gmail.com>
2026-08-19 19:20:59 +0300

    Update SelfPropellingBoatVelocityMixin.java

--- src/main/java/com/minecartmagic/mixin/SelfPropellingBoatVelocityMixin.java
+++ src/main/java/com/minecartmagic/mixin/SelfPropellingBoatVelocityMixin.java
@@ -1,5 +1,6 @@
 package com.minecartmagic.mixin;
 
+import com.minecartmagic.ModEnchantments;
 import com.minecartmagic.entity.SelfPropellingBoatEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,17 +22,65 @@ public abstract class SelfPropellingBoatVelocityMixin {
                 (BoatEntity) (Object) this;
 
         /*
-         * Только наша самоходная лодка.
+         * Работаем только с нашей самоходной лодкой.
          */
         if (!(boat instanceof SelfPropellingBoatEntity selfPropellingBoat)) {
             return;
         }
 
         /*
-         * Вся логика двигателя находится только
-         * в SelfPropellingBoatEntity.applySelfPropulsion().
+         * Нет топлива -> двигатель выключен.
+         */
+        if (!selfPropellingBoat.hasFuel()) {
+            return;
+        }
+
+        /*
+         * На суше двигатель не тянет.
+         */
+        if (!selfPropellingBoat.isTouchingWater()) {
+            return;
+        }
+
+        /*
+         * КРИТИЧЕСКОЕ МЕСТО.
          *
-         * Здесь не дублируем расчёт скорости.
+         * Перед каждым расчётом двигателя берём НАСТОЯЩИЙ
+         * уровень minecartmagic:tailwind с entity.
+         *
+         * 0 = нет зачарования
+         * 1 = Tailwind I
+         * 2 = Tailwind II
+         * 3 = Tailwind III
+         */
+        int tailwindLevel =
+                ModEnchantments.getTailwindLevel(
+                        selfPropellingBoat
+                );
+
+        /*
+         * Если attachment уже содержит зачарование,
+         * обновляем синхронизированное значение двигателя.
+         */
+        if (tailwindLevel > 0) {
+            selfPropellingBoat.setEngineTailwindLevel(
+                    Math.min(
+                            3,
+                            tailwindLevel
+                    )
+            );
+        }
+
+        /*
+         * Теперь applySelfPropulsion() получает
+         * актуальный уровень Попутного ветра.
+         *
+         * Его собственная формула:
+         *
+         * 0  = 0.45
+         * I  = 0.62
+         * II = 0.72
+         * III= 0.84
          */
         selfPropellingBoat.applySelfPropulsion();
     }
