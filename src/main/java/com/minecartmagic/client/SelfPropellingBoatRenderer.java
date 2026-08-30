7005e128d94a38dc41dd09215a3b5317db2d8d40
Morty <zaharzvagin254@gmail.com>
2026-08-30 17:20:59 +0300

    Update SelfPropellingBoatRenderer.java

--- src/main/java/com/minecartmagic/client/SelfPropellingBoatRenderer.java
+++ src/main/java/com/minecartmagic/client/SelfPropellingBoatRenderer.java
@@ -1,7 +1,10 @@
 package com.minecartmagic.client;
 
 import com.minecartmagic.entity.SelfPropellingBoatEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.RotationAxis;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class SelfPropellingBoatRenderer
@@ -16,5 +19,53 @@ public class SelfPropellingBoatRenderer
         );
 
         shadowRadius = 0.8F;
+    }
+
+    @Override
+    public void render(
+            SelfPropellingBoatEntity entity,
+            float entityYaw,
+            float tickDelta,
+            MatrixStack matrices,
+            VertexConsumerProvider vertexConsumers,
+            int light
+    ) {
+        /*
+         * GeckoLib уже выполняет свой стандартный поворот:
+         *
+         * 180° - entityYaw
+         *
+         * Наша .geo-модель ориентирована относительно другой
+         * локальной оси, поэтому дополнительно разворачиваем
+         * модель на 180°.
+         *
+         * Итоговая ориентация:
+         *
+         * (180° - yaw) + 180°
+         * = -yaw
+         *
+         * То есть модель теперь ориентируется так же,
+         * как сама BoatEntity.
+         *
+         * Никакую физику лодки это не меняет.
+         */
+        matrices.push();
+
+        matrices.multiply(
+                RotationAxis.POSITIVE_Y.rotationDegrees(
+                        180.0F
+                )
+        );
+
+        super.render(
+                entity,
+                entityYaw,
+                tickDelta,
+                matrices,
+                vertexConsumers,
+                light
+        );
+
+        matrices.pop();
     }
 }
