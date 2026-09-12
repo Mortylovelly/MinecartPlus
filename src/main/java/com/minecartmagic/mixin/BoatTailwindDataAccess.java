e92b86dad0db2dc7512bbcef6c6b33962d76a9bc
Morty <zaharzvagin254@gmail.com>
2026-09-13 02:43:25 +0300

    Add synced 1.20.1 Tailwind data access

---
+++ src/main/java/com/minecartmagic/mixin/BoatTailwindDataAccess.java
@@ -0,0 +1,8 @@
+package com.minecartmagic.mixin;
+
+public interface BoatTailwindDataAccess {
+
+    int minecartmagic$getTailwindDataLevel();
+
+    void minecartmagic$setTailwindDataLevel(int level);
+}
