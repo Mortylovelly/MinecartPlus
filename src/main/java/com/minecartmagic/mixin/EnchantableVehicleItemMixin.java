d014688ab5f859be4afd51931138cf42469001a6
Morty <zaharzvagin254@gmail.com>
2026-09-13 14:00:39 +0300

    Match original enchantable item targets

--- src/main/java/com/minecartmagic/mixin/EnchantableVehicleItemMixin.java
+++ src/main/java/com/minecartmagic/mixin/EnchantableVehicleItemMixin.java
@@ -1,11 +1,11 @@
 package com.minecartmagic.mixin;
 
-import com.minecartmagic.item.AdvancedMinecartItem;
-import com.minecartmagic.item.SelfPropellingBoatItem;
+import com.minecartmagic.ModItems;
 import net.minecraft.item.BoatItem;
+import net.minecraft.item.ChestBoatItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.MinecartItem;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,32 +15,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class EnchantableVehicleItemMixin {
 
     @Inject(method = "isEnchantable", at = @At("RETURN"), cancellable = true)
-    private void minecartmagic$makeVehicleEnchantable(
+    private void minecartmagic$makeOriginalTargetEnchantable(
             ItemStack stack,
             CallbackInfoReturnable<Boolean> cir
     ) {
-        Item item = (Item) (Object) this;
-
-        if (minecartmagic$isEnchantableVehicle(item)) {
+        if (minecartmagic$isOriginalEnchantableTarget(stack)) {
             cir.setReturnValue(true);
         }
     }
 
     @Inject(method = "getEnchantability", at = @At("RETURN"), cancellable = true)
-    private void minecartmagic$giveVehicleEnchantability(
+    private void minecartmagic$giveOriginalEnchantability(
             CallbackInfoReturnable<Integer> cir
     ) {
         Item item = (Item) (Object) this;
 
-        if (minecartmagic$isEnchantableVehicle(item)) {
+        if (item == Items.MINECART
+                || item instanceof BoatItem
+                || item instanceof ChestBoatItem
+                || item == ModItems.SELF_PROPELLING_BOAT) {
             cir.setReturnValue(15);
         }
     }
 
-    private static boolean minecartmagic$isEnchantableVehicle(Item item) {
-        return item instanceof BoatItem
-                || item instanceof MinecartItem
-                || item instanceof SelfPropellingBoatItem
-                || item instanceof AdvancedMinecartItem;
+    private static boolean minecartmagic$isOriginalEnchantableTarget(ItemStack stack) {
+        Item item = stack.getItem();
+
+        return item == Items.MINECART
+                || item instanceof BoatItem
+                || item instanceof ChestBoatItem
+                || item == ModItems.SELF_PROPELLING_BOAT;
     }
 }
