a4d98a7f89d42dbeab460fbc9c2582322355767a
Morty <zaharzvagin254@gmail.com>
2026-09-13 14:16:02 +0300

    Make exactly the original supported vehicle items enchantable

--- src/main/java/com/minecartmagic/mixin/EnchantableVehicleItemMixin.java
+++ src/main/java/com/minecartmagic/mixin/EnchantableVehicleItemMixin.java
@@ -1,6 +1,6 @@
 package com.minecartmagic.mixin;
 
-import com.minecartmagic.ModItems;
+import com.minecartmagic.item.SelfPropellingBoatItem;
 import net.minecraft.item.BoatItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -29,18 +29,9 @@ public abstract class EnchantableVehicleItemMixin {
     ) {
         Item item = (Item) (Object) this;
 
-        if (item == Items.MINECART
-                || item instanceof BoatItem
-                || item == Items.OAK_CHEST_BOAT
-                || item == Items.SPRUCE_CHEST_BOAT
-                || item == Items.BIRCH_CHEST_BOAT
-                || item == Items.JUNGLE_CHEST_BOAT
-                || item == Items.ACACIA_CHEST_BOAT
-                || item == Items.DARK_OAK_CHEST_BOAT
-                || item == Items.MANGROVE_CHEST_BOAT
-                || item == Items.CHERRY_CHEST_BOAT
-                || item == Items.BAMBOO_CHEST_RAFT
-                || item == ModItems.SELF_PROPELLING_BOAT) {
+        if (minecartmagic$isOriginalEnchantableTarget(
+                new ItemStack(item)
+        )) {
             cir.setReturnValue(15);
         }
     }
@@ -59,6 +50,6 @@ public abstract class EnchantableVehicleItemMixin {
                 || item == Items.MANGROVE_CHEST_BOAT
                 || item == Items.CHERRY_CHEST_BOAT
                 || item == Items.BAMBOO_CHEST_RAFT
-                || item == ModItems.SELF_PROPELLING_BOAT;
+                || item instanceof SelfPropellingBoatItem;
     }
 }
