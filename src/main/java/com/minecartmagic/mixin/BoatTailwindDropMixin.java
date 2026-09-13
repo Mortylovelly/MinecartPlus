ca8a1d94272693ac507c257898b792adc0688fdb
Morty <zaharzvagin254@gmail.com>
2026-09-13 03:12:08 +0300

    Cover normal and chest boat drops on 1.20.1

--- src/main/java/com/minecartmagic/mixin/BoatTailwindDropMixin.java
+++ src/main/java/com/minecartmagic/mixin/BoatTailwindDropMixin.java
@@ -4,6 +4,7 @@ import com.minecartmagic.ModEnchantments;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.ChestBoatEntity;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registry;
@@ -13,7 +14,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(BoatEntity.class)
+@Mixin({
+        BoatEntity.class,
+        ChestBoatEntity.class
+})
 public abstract class BoatTailwindDropMixin {
 
     @Redirect(
@@ -21,9 +25,32 @@ public abstract class BoatTailwindDropMixin {
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/vehicle/BoatEntity;dropItem(Lnet/minecraft/item/ItemConvertible;)Lnet/minecraft/entity/ItemEntity;"
-            )
+            ),
+            require = 0
     )
-    private ItemEntity minecartmagic$enchantDroppedBoat(
+    private ItemEntity minecartmagic$enchantDroppedBoatFromBoat(
+            BoatEntity boat,
+            ItemConvertible item
+    ) {
+        return minecartmagic$dropEnchanted(boat, item);
+    }
+
+    @Redirect(
+            method = "dropItems(Lnet/minecraft/entity/damage/DamageSource;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/vehicle/ChestBoatEntity;dropItem(Lnet/minecraft/item/ItemConvertible;)Lnet/minecraft/entity/ItemEntity;"
+            ),
+            require = 0
+    )
+    private ItemEntity minecartmagic$enchantDroppedBoatFromChestBoat(
+            ChestBoatEntity boat,
+            ItemConvertible item
+    ) {
+        return minecartmagic$dropEnchanted(boat, item);
+    }
+
+    private static ItemEntity minecartmagic$dropEnchanted(
             BoatEntity boat,
             ItemConvertible item
     ) {
