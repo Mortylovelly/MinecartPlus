fc3ca936ebb4230c6475150d63fa5b564df8ce9c
Morty <zaharzvagin254@gmail.com>
2026-09-13 14:27:36 +0300

    Match original Tailwind supported items exactly

--- src/main/java/com/minecartmagic/LegacyEnchantments.java
+++ src/main/java/com/minecartmagic/LegacyEnchantments.java
@@ -1,6 +1,5 @@
 package com.minecartmagic;
 
-import com.minecartmagic.item.SelfPropellingBoatItem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
@@ -81,7 +80,7 @@ public final class LegacyEnchantments {
                         || item == Items.MANGROVE_CHEST_BOAT
                         || item == Items.CHERRY_CHEST_BOAT
                         || item == Items.BAMBOO_CHEST_RAFT
-                        || item instanceof SelfPropellingBoatItem;
+                        || item == ModItems.SELF_PROPELLING_BOAT;
             }
         };
     }
